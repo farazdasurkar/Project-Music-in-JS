ce96bf80e20eca8c2aa06556f530110f54dcf428
farazdasurkar <farazdasurkar@users.noreply.github.com>
2021-11-13 22:36:36 +0530

    commit

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i <= numberOfDrumButtons; i++) {
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInner = this.innerHTML;
         makesound(buttonInner);
-
-
-
     }
     );
 }
@@ -20,37 +17,37 @@ document.addEventListener("keypress", function (event) {
 
 function makesound(key) {
     switch (key) {
-        case "Sa":
+        case "s":
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
             break;
 
-        case "Re":
+        case "r":
             var tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
             break;
 
-        case "Ga":
+        case "g":
             var tom3 = new Audio('sounds/tom-3.mp3');
             tom3.play();
             break;
 
-        case "Ma":
+        case "m":
             var tom4 = new Audio('sounds/tom-4.mp3');
             tom4.play();
             break;
 
-        case "Pa":
+        case "p":
             var snare = new Audio('sounds/snare.mp3');
             snare.play();
             break;
 
-        case "Da":
+        case "d":
             var crash = new Audio('sounds/crash.mp3');
             crash.play();
             break;
 
-        case "Ni":
+        case "n":
             var kick = new Audio('sounds/kick-bass.mp3');
             kick.play();
             break;
